45e5fd04c15f6f47beff3b867db4a95c9812539b
Eldar Shamukhamedov <eldarshamukhamedov@pm.me>
2025-11-07 15:43:44 -0500

    Add pin/close-all-but-current-and-pinned keybinds

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -5,6 +5,7 @@ return {
 		require("barbar").setup({
 			icons = {
 				modified = { button = "" },
+				pinned = { filename = true },
 			},
 
 			sidebar_filetypes = {
@@ -15,7 +16,14 @@ return {
 		-- Move between buffers
 		vim.keymap.set("n", "<leader>.", "<cmd>BufferNext<cr>", { desc = "Go to next buffer" })
 		vim.keymap.set("n", "<leader>,", "<cmd>BufferPrevious<cr>", { desc = "Go to previous buffer" })
+		vim.keymap.set("n", "<leader>p", "<cmd>BufferPin<cr>", { desc = "Pin buffer" })
 		vim.keymap.set("n", "<leader>x", "<cmd>BufferClose<cr>", { desc = "Close buffer" })
+		vim.keymap.set(
+			"n",
+			"<leader>X",
+			"<cmd>BufferCloseAllButCurrentOrPinned<cr>",
+			{ desc = "Close all but current or pinned" }
+		)
 		vim.keymap.set("n", "<leader>n", "<cmd>enew<cr>", { desc = "Open new buffer" })
 	end,
 }
